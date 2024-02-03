92b226a9cd0400160e3a8f4077d89499e3923392
trishaDas13 <trisha.das1308@gmail.com>
2024-02-03 18:15:04 +0530

    header and routing done

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,23 +1,82 @@
 import React, { useRef } from "react";
 import {
-    BsFillArrowLeftCircleFill,
-    BsFillArrowRightCircleFill,
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
 } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
-
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImg/Img";
 import PosterFallback from "../../assets/no-poster.png";
-
 import "./style.scss";
+import CircleRating from '../circleRating/CircleRating'
 
+const Carousel = ({ data, loading }) => {
+  const carouselContainer = useRef();
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
 
-const Carousel = ({data, loading}) => {
+  const navigation = (dir) => {};
+
+  const skItems = () =>{
+    return(
+      <div className="skeletonItem">
+                <div className="posterBlock skeleton"></div>
+                <div className="textBlock">
+                    <div className="title skeleton"></div>
+                    <div className="date skeleton"></div>
+                </div>
+            </div>
+    )
+  }
+
   return (
-    <div>Carousel</div>
-  )
-}
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRighttNav arrow"
+          onClick={() => navigation("right")}
+        />
+        {!loading ? (
+          <div className="carouselItems">
+            {data?.map((item) => {
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : PosterFallback;
+              return (
+                <div className="carouselItem" key={item.id}>
+                  <div className="posterBlock">
+                    <Img src={posterUrl} />
+                    <CircleRating
+                      rating = {item.vote_average.toFixed(1)}
+                    />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{item.title || item.name}</span>
+                    <span className="date">{dayjs(item.release_Date).format("MMM D, YYYY")}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+            {skItems()}
+            {skItems()}
+            {skItems()}
+            {skItems()}
+            {skItems()}
+            {skItems()}
+          </div>
+        )}
+      </ContentWrapper>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
